4f8c327d7ae5c323b4c00bd04c04d444aad20501
yukihirop <te108186@gmail.com>
2018-12-18 03:07:01 +0900

    Configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: 'build.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  performance: {
+    hints: 'error'
+  },
+  devtool: '#eval-source-map'
+}
+
+// http://vue-loader.vuejs.org/en/workflow/production.html
+if (process.env.NODE_ENV === 'production'){
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    })
+  ])
+}
